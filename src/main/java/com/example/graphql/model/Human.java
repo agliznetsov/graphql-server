0cdf3fbf3c4ba45c27d4b18d242d848891728153
Andrey Gliznetsov <andrey.gliznetsov@collibra.com>
2019-06-07 11:40:35 +0200

    first commit

---
+++ src/main/java/com/example/graphql/model/Human.java
@@ -0,0 +1,24 @@
+package com.example.graphql.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Human extends FilmCharacter {
+    private String homePlanet;
+
+    public String getHomePlanet() {
+        return homePlanet;
+    }
+
+    public void setHomePlanet(String homePlanet) {
+        this.homePlanet = homePlanet;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "id='" + this.getId() + '\'' +
+                ", name='" + this.getName() + '\'' +
+                '}';
+    }
+}
